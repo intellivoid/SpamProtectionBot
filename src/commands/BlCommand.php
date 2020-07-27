39525ee48d8afbdf1598f81c0957e4be171db416
netkas <netkas@intellivoid.info>
2020-07-27 04:13:58 -0400

    Added alias BlCommand->BlacklistCommand

---
+++ src/commands/BlCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+    /** @noinspection PhpUndefinedClassInspection */
+    /** @noinspection PhpUnused */
+    /** @noinspection PhpIllegalPsrClassPathInspection */
+
+    namespace Longman\TelegramBot\Commands\UserCommands;
+
+    use Longman\TelegramBot\Commands\UserCommand;
+    use Longman\TelegramBot\Entities\ServerResponse;
+    use Longman\TelegramBot\Exception\TelegramException;
+    use TelegramClientManager\Exceptions\DatabaseException;
+    use TelegramClientManager\Exceptions\InvalidSearchMethod;
+    use TelegramClientManager\Exceptions\TelegramClientNotFoundException;
+
+    /**
+     * Blacklist user command aias
+     */
+    class BlCommand extends UserCommand
+    {
+        /**
+         * @var string
+         */
+        protected $name = 'blacklist alias';
+
+        /**
+         * @var string
+         */
+        protected $description = 'Alias to the blacklist command';
+
+        /**
+         * @var string
+         */
+        protected $usage = '/blacklist alias';
+
+        /**
+         * @var string
+         */
+        protected $version = '1.0.0';
+
+        /**
+         * @var bool
+         */
+        protected $private_only = false;
+
+        /**
+         * Command execute method
+         *
+         * @return ServerResponse|null
+         * @throws DatabaseException
+         * @throws InvalidSearchMethod
+         * @throws TelegramClientNotFoundException
+         * @throws TelegramException
+         * @noinspection DuplicatedCode
+         */
+        public function execute()
+        {
+            $BlacklistCommand = new BlacklistCommand($this->telegram, $this->update);
+            return $BlacklistCommand->execute();
+        }
+
+    }
