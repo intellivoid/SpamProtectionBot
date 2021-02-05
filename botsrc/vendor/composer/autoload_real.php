29431f5a21c77eff93f6b5e0350cb97666ca2094
netkas <netkas@intellivoid.net>
2021-02-05 04:44:15 -0500

    Bug fix?

--- botsrc/vendor/composer/autoload_real.php
+++ botsrc/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd1fbdec1c276e9fbf3175ff04121f7e5
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
