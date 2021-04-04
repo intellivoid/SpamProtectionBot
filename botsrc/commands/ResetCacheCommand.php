4e88d4be93ead82ccf00a9d2d3b36e649ac3d699
Zi Xing <zixing.narrakas1996@gmail.com>
2021-04-04 13:33:54 -0400

    Added ResetCacheCommand

---
+++ botsrc/commands/ResetCacheCommand.php
@@ -0,0 +1,166 @@
+<?php
+
+    /** @noinspection PhpUndefinedClassInspection */
+    /** @noinspection PhpUnused */
+    /** @noinspection PhpIllegalPsrClassPathInspection */
+
+    namespace Longman\TelegramBot\Commands\UserCommands;
+
+    use Longman\TelegramBot\Commands\UserCommand;
+    use Longman\TelegramBot\Entities\Message;
+    use Longman\TelegramBot\Entities\ServerResponse;
+    use Longman\TelegramBot\Exception\TelegramException;
+    use Longman\TelegramBot\Request;
+    use pop\pop;
+    use SpamProtection\Managers\SettingsManager;
+    use SpamProtection\Objects\TelegramObjects\ChatMember;
+    use SpamProtection\Utilities\Hashing;
+    use SpamProtectionBot;
+    use TelegramClientManager\Abstracts\SearchMethods\TelegramClientSearchMethod;
+    use TelegramClientManager\Abstracts\TelegramChatType;
+    use TelegramClientManager\Exceptions\DatabaseException;
+    use TelegramClientManager\Exceptions\InvalidSearchMethod;
+    use TelegramClientManager\Exceptions\TelegramClientNotFoundException;
+    use VerboseAdventure\Abstracts\EventType;
+
+    /**
+     * Rest Cache Command
+     *
+     * Allows a user to reset the cache of a group after x amount of time.
+     */
+    class ResetCacheCommand extends UserCommand
+    {
+        /**
+         * @var string
+         */
+        protected $name = 'ResetCache';
+
+        /**
+         * @var string
+         */
+        protected $description = 'Allows a user to reset the cache of a group after x amount of time';
+
+        /**
+         * @var string
+         */
+        protected $usage = '/ResetCache';
+
+        /**
+         * @var string
+         */
+        protected $version = '1.0.0';
+
+        /**
+         * @var bool
+         */
+        protected $private_only = false;
+
+        /**
+         * The whois command used for finding targets
+         *
+         * @var WhoisCommand|null
+         */
+        public $WhoisCommand = null;
+
+        /**
+         * Command execute method
+         *
+         * @return ServerResponse
+         * @throws DatabaseException
+         * @throws InvalidSearchMethod
+         * @throws TelegramException
+         * @throws TelegramClientNotFoundException
+         * @noinspection DuplicatedCode
+         */
+        public function execute()
+        {
+            // Find clients
+            $TelegramClientManager = SpamProtectionBot::getTelegramClientManager();
+            $this->WhoisCommand = new WhoisCommand($this->telegram, $this->update);
+            $this->WhoisCommand->findClients();
+
+            // Tally DeepAnalytics
+            $DeepAnalytics = SpamProtectionBot::getDeepAnalytics();
+            $DeepAnalytics->tally('tg_spam_protection', 'messages', 0);
+            $DeepAnalytics->tally('tg_spam_protection', 'reset_cache', 0);
+            $DeepAnalytics->tally('tg_spam_protection', 'messages', (int)$this->WhoisCommand->ChatObject->ID);
+            $DeepAnalytics->tally('tg_spam_protection', 'reset_cache', (int)$this->WhoisCommand->ChatObject->ID);
+
+            // Ignore forwarded commands
+            if($this->getMessage()->getForwardFrom() !== null || $this->getMessage()->getForwardFromChat())
+            {
+                return null;
+            }
+
+            $ChatClient = $this->WhoisCommand->ChatClient;
+            $ChatSettings = SettingsManager::getChatSettings($ChatClient);
+
+            try
+            {
+                // Update the admin cache if outdated
+                /** @noinspection DuplicatedCode */
+                if($ChatClient->Chat->Type == TelegramChatType::Group || $ChatClient->Chat->Type == TelegramChatType::SuperGroup)
+                {
+                    if(((int)time() - $ChatSettings->AdminCacheLastUpdated) > 120)
+                    {
+                        $Results = Request::getChatAdministrators(["chat_id" => $ChatClient->Chat->ID]);
+
+                        if($Results->isOk())
+                        {
+                            /** @var array $ChatMembersResponse */
+                            $ChatMembersResponse = $Results->getRawData()["result"];
+                            $ChatSettings->Administrators = array();
+                            $ChatSettings->AdminCacheLastUpdated = (int)time();
+
+                            foreach($ChatMembersResponse as $chatMember)
+                            {
+                                $ChatSettings->Administrators[] = ChatMember::fromArray($chatMember);
+                            }
+
+                            $this->WhoisCommand->ChatClient = SettingsManager::updateChatSettings($ChatClient, $ChatSettings);
+                            SpamProtectionBot::getTelegramClientManager()->getTelegramClientManager()->updateClient($ChatClient);
+
+                            return Request::sendMessage([
+                                "chat_id" => $this->getMessage()->getChat()->getId(),
+                                "reply_to_message_id" => $this->getMessage()->getMessageId(),
+                                "parse_mode" => "html",
+                                "text" => LanguageCommand::localizeChatText($this->WhoisCommand, "Success, cache values has been reset. " . count($ChatSettings->Administrators) . " administrator(s) found.")
+                            ]);
+                        }
+                        else
+                        {
+                            return Request::sendMessage([
+                                "chat_id" => $this->getMessage()->getChat()->getId(),
+                                "reply_to_message_id" => $this->getMessage()->getMessageId(),
+                                "parse_mode" => "html",
+                                "text" => LanguageCommand::localizeChatText($this->WhoisCommand, "There was an error while trying to request the admin list from Telegram.")
+                            ]);
+                        }
+                    }
+                    else
+                    {
+                        $TimeLeft = abs((time() - $ChatSettings->AdminCacheLastUpdated) - 120);
+
+                        return Request::sendMessage([
+                            "chat_id" => $this->getMessage()->getChat()->getId(),
+                            "reply_to_message_id" => $this->getMessage()->getMessageId(),
+                            "parse_mode" => "html",
+                            "text" => LanguageCommand::localizeChatText($this->WhoisCommand, "The cache has been updated recently. Try again in $TimeLeft second(s)")
+                        ]);
+                    }
+                }
+            }
+            catch(Exception $e)
+            {
+                SpamProtectionBot::getLogHandler()->log(EventType::WARNING, "There was an error while trying to process handleMessage (AdminCacheUpdate)", "handleMessage");
+                SpamProtectionBot::getLogHandler()->logException($e, "handleMessage");
+            }
+
+            return Request::sendMessage([
+                "chat_id" => $this->getMessage()->getChat()->getId(),
+                "reply_to_message_id" => $this->getMessage()->getMessageId(),
+                "parse_mode" => "html",
+                "text" => LanguageCommand::localizeChatText($this->WhoisCommand, "There was an unknown issue while trying to update the admin cache.")
+            ]);
+        }
+    }
